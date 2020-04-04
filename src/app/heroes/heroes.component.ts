b8365683ccd6d98b794f3b7e8b09385c8227f7a4
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2020-04-04 22:38:14 +0300

    init

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
+import { Hero } from "../hero";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.scss"],
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id = ${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes;
+    });
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+}
